2601d5f8e59662361bc0f24f75822ca8766f3df7
StarostenkoA <167009338+StarostenkoA@users.noreply.github.com>
2024-04-29 15:13:20 +0300

    Update lesson5_2.py

--- lesson5/lesson5_2.py
+++ lesson5/lesson5_2.py
@@ -1,1 +1,9 @@
-
+users=[
+    {'FIO':'Starostenko','d':'Capt','b':'19750113','n':['C#','Python','Asm','C++'],'children':[{'name':'Ilya','b':"20030516"},{'name':'Lena','b':"20120118"}]},
+    {'FIO':'Konon','d':'Capt','b':'19810214','n':['Python'],'children':[{'name':'Don','b':"20180707"}]},
+    {'FIO':'Mel','d':'Capt','b':'19890315','n':['Python','Pascal'],'children':[{'name':'Vas','b':"20190816"}]}
+    ]
+name = 'Starostenko'#input('Enter name:')
+for person in users:
+    if person['FIO'] == name:
+        print(person)
