afe4cf57244cfa6fdcd92e8a464b7ce6fb44b0d8
StarostenkoA <167009338+StarostenkoA@users.noreply.github.com>
2024-04-29 15:16:37 +0300

    Update lesson5_5.py

--- lesson5/lesson5_5.py
+++ lesson5/lesson5_5.py
@@ -1,1 +1,10 @@
+string=input("enter str:")
+set1 = set(string)
+print(f"count unique chars={len(set1)}")
 
+set2 = set(string.split())
+print(f"count unique words={len(set2)}")
+
+from collections import Counter
+c = Counter(string)
+print(f"most common {c.most_common(1)[0][0]}")
