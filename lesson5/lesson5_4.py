96ec96b41086a47e55472991be10d60597d00f4d
StarostenkoA <167009338+StarostenkoA@users.noreply.github.com>
2024-04-29 15:16:01 +0300

    Update lesson5_4.py

--- lesson5/lesson5_4.py
+++ lesson5/lesson5_4.py
@@ -1,1 +1,14 @@
-
+string=input("enter a, b, c, d:")
+sum=0
+for char in string:
+    if char == "a":
+        sum+=10
+    elif char == "b":
+        sum+=20
+    elif char == "c":
+        sum+=30
+    elif char == "d":
+        sum+=40
+    else:
+        print(f"{char} - Unknown char")
+print(f"All score={sum}")
