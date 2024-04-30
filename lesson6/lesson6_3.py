0fa2c4d5685144eb1878626b1e8363c558626b56
StarostenkoA <167009338+StarostenkoA@users.noreply.github.com>
2024-04-30 09:30:40 +0300

    Update lesson6_3.py

--- lesson6/lesson6_3.py
+++ lesson6/lesson6_3.py
@@ -1,1 +1,11 @@
-
+a,b,c,=input("enter 3 digit:").split()
+if (int(a)>=int(b)):
+    if(int(a)>=int(c)):
+        print(a)
+    else:
+        print(c)
+else:
+    if(int(b)>=int(c)):
+        print(b)
+    else:
+        print(c)
