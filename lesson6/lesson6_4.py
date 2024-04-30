49bdbdd7a8ce9db9cb92c7e37f1c51e19cbe8554
StarostenkoA <167009338+StarostenkoA@users.noreply.github.com>
2024-04-30 09:31:12 +0300

    Update lesson6_4.py

--- lesson6/lesson6_4.py
+++ lesson6/lesson6_4.py
@@ -1,1 +1,12 @@
-
+string=input("Enter num:")
+num=int(string)
+count=0
+if(num<20):
+    for n in range(1, num+1):
+        if (n%7 == 0):
+            count+=1
+if(num>20):
+    for n in range(1, num+1):
+        if (n%11 == 0):
+            count+=1
+print(count)
