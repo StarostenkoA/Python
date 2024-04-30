7a56437d70548e0d6d1e4f4660f4246b96c1679b
StarostenkoA <167009338+StarostenkoA@users.noreply.github.com>
2024-04-30 09:32:16 +0300

    Update lesson6_6.py

--- lesson6/lesson6_6.py
+++ lesson6/lesson6_6.py
@@ -1,1 +1,13 @@
+a1=356
+a2=4
+a3=45
+a4=78.34
 
+if isinstance(a1, float) and isinstance(a2, float) and isinstance(a3, float) and isinstance(a4, float):
+    print("True, all float")
+
+if isinstance(a1, str) or isinstance(a2, str) or isinstance(a3, str) or isinstance(a4, str):
+    print("True, >1 string")
+    
+if isinstance(a1, int) and isinstance(a3, int) or isinstance(a2, int) and isinstance(a4, int) or isinstance(a3, int) and isinstance(a4, int):
+    print("True, para int")
