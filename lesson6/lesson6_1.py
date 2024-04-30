364b982094d4ae241289e1132092383780d080a9
StarostenkoA <167009338+StarostenkoA@users.noreply.github.com>
2024-04-30 09:29:21 +0300

    Update lesson6_1.py

--- lesson6/lesson6_1.py
+++ lesson6/lesson6_1.py
@@ -1,1 +1,17 @@
+string=input("enter Year:")
+year = int(string)
+if year >= 2020 and year<2025 :
+    print("REBENOK")
+elif year >= 2016 and year<2020 :
+    print("PODROSTOK")
+elif year >= 2010 and year<2016 :
+    print("JUNOSHA")
+elif year >= 1996 and year<2010 :
+    print("RACVET SIL")
+elif year >= 1965 and year<1996 :
+    print("POJILOY")
+elif year >= 1925 and year<1965 :
+    print("STARIK")
+else:
+    print(f"{year} - Bad year")
 
