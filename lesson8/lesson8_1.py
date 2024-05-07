a67daee2f5bb598996b8c30e4d7d1f5f9426910c
StarostenkoA <167009338+StarostenkoA@users.noreply.github.com>
2024-05-07 10:36:11 +0300

    Update lesson8_1.py

--- lesson8/lesson8_1.py
+++ lesson8/lesson8_1.py
@@ -1,1 +1,11 @@
+def print_fio(fio, direct=False):
+    f=fio.split()
+    if direct:
+        print(f"{f[0]} {f[1][0]}.{f[2][0]}.")
+    else:
+        print(f"{f[1][0]}.{f[2][0]}.{f[0]}")
 
+
+fio="Starostenko Alelsandr Vasilievich"#input("Enter Fio:")
+print_fio("Starostenko Alelsandr Vasilievich")  
+print_fio("Starostenko Alelsandr Vasilievich",direct=True) 
