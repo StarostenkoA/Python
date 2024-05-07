5cae0590a5c4a5f2fcae3c0c53162df4151ec4f4
StarostenkoA <167009338+StarostenkoA@users.noreply.github.com>
2024-05-07 10:39:10 +0300

    Update lesson8_6.py

--- lesson8/lesson8_6.py
+++ lesson8/lesson8_6.py
@@ -1,1 +1,18 @@
+def yes_or_no(list):
+    
+    for i in list:
+        if(isinstance(i,int)!=True):
+            return False
 
+    f=dict()
+    for i in list:
+        if f.get(i):
+            f[i]='no'
+        else:
+            f.pop(i='yes')
+            
+    print (f)
+    return f.values()
+        
+
+print(yes_or_no([1,2,3,1,4]))
