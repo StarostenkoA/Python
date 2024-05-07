4a5366e80b2991a2aea9ede1b4969e874dd37f25
StarostenkoA <167009338+StarostenkoA@users.noreply.github.com>
2024-05-07 09:23:44 +0300

    Update lesson7_4.py

--- lesson7/lesson7_4.py
+++ lesson7/lesson7_4.py
@@ -1,1 +1,15 @@
+SPACE = ' '
+STRAR = '*'
 
+rows = int(input("Enter num rows (3-20):"))
+spaces = rows-1
+stars = 1
+
+for i in range(rows):
+    print(
+        (SPACE*spaces) +
+        (STRAR*stars) +
+        (SPACE*spaces)
+    )
+    stars += 2
+    spaces -= 1
