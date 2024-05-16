e2fddbedb77d42c19272465145b6fbff2e24cf4a
StarostenkoA <167009338+StarostenkoA@users.noreply.github.com>
2024-05-16 10:10:05 +0300

    Update lesson9_3.py

--- lesson9/lesson9_3.py
+++ lesson9/lesson9_3.py
@@ -1,1 +1,15 @@
+class MyExceptionFact(Exception):
+    pass
 
+def fact(n):
+    if 0 <= n <= 1:
+        return 1
+    if n<0:
+        raise MyExceptionFact("n<0")
+    return fact(n - 1) * n
+
+n=5
+try:
+    print(f"{n}! = {fact(n)}")
+except (MyExceptionFact) as e:
+    print(f"{e} {n}! does not exist")
