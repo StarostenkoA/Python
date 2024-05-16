60d09781c769db5c3737dbefa413f95f4db04f96
StarostenkoA <167009338+StarostenkoA@users.noreply.github.com>
2024-05-16 10:11:20 +0300

    Update lesson9_5.py

--- lesson9/lesson9_5.py
+++ lesson9/lesson9_5.py
@@ -1,1 +1,16 @@
+some_list1 = [1, 2, 3, [4, [5, 6], 7], 8, 9]
+some_list2=[1,[2,[[3],4]],5,[[[6,7]]],8,[[[[9,10]],11]],12]
 
+
+level=-1
+def PrintList(MyList, Char="-"):
+    global level
+    level+=1
+    for item in MyList:
+        if isinstance(item, list):
+            PrintList(item)
+        else:
+            print((Char*level),item) 
+    level-=1
+
+PrintList(some_list2)
