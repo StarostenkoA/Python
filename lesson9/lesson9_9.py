3d778b852ea7695eaaf0194ba6264994f372a589
StarostenkoA <167009338+StarostenkoA@users.noreply.github.com>
2024-05-16 10:13:56 +0300

    Update lesson9_9.py

--- lesson9/lesson9_9.py
+++ lesson9/lesson9_9.py
@@ -1,1 +1,12 @@
+Country = {
+    "Belarus":["Minsk","Grodno","Brest"],
+    "Ukraine":["Kyiv","Kharkiv","Lviv"]
+    }
 
+city = input("Enter city:")
+for country, cities in Country.items():
+    if city in cities:
+        print(f"{city} is a city in {country}")
+        break
+else:
+    print(f"Country for {city} not found")
