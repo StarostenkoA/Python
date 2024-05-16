92faaafc3dde4451dad4ef641ad9469fadb97df6
StarostenkoA <167009338+StarostenkoA@users.noreply.github.com>
2024-05-16 10:19:19 +0300

    Update lesson9_9.py

--- lesson9/lesson9_9.py
+++ lesson9/lesson9_9.py
@@ -8,5 +8,5 @@ for country, cities in Country.items():
     if city in cities:
         print(f"{city} is a city in {country}")
         break
-    else:
-        print(f"Country for {city} not found")
+else:
+    print(f"Country for {city} not found")
