c882b4fb70864ff9677666d61c18a711e69af95e
StarostenkoA <167009338+StarostenkoA@users.noreply.github.com>
2024-05-16 10:12:15 +0300

    Update lesson9_6.py

--- lesson9/lesson9_6.py
+++ lesson9/lesson9_6.py
@@ -1,1 +1,5 @@
+DaysTemp={"day1":18, "day2":22, "day3":7, "day4":11, "day5":14}
 
+print(f"init - {DaysTemp}")
+print(f"sorted - {sorted(DaysTemp.values(),key=lambda x:x)}")
+print(f"sorted desc - {sorted(DaysTemp.values(),key=lambda x:x, reverse=True)}")
