fef5b5c8c9b96dd21f291790ff51fcc9609107da
StarostenkoA <167009338+StarostenkoA@users.noreply.github.com>
2024-05-16 10:10:39 +0300

    Update lesson9_4.py

--- lesson9/lesson9_4.py
+++ lesson9/lesson9_4.py
@@ -1,1 +1,9 @@
+numbers  = [1,2,3,4,5,6,7,8,9]
+list1=list(map(lambda x:x**2,numbers))
+list2=list(map(lambda x: x if x % 2 else x+3,numbers))
+list3=list(map(lambda x: x*3 if x % 2 else x*2,numbers))
 
+print(numbers)
+print(list1)
+print(list2)
+print(list3)
