3f65bd2db0961f9404f4284d66a6f7f31fe59ae4
StarostenkoA <167009338+StarostenkoA@users.noreply.github.com>
2024-05-16 10:13:23 +0300

    Update lesson9_8.py

--- lesson9/lesson9_8.py
+++ lesson9/lesson9_8.py
@@ -1,1 +1,6 @@
+MyList=['string',12,True]
 
+print(type(MyList[1]))
+
+print(list(filter(lambda x:True if isinstance(x, str) else False, MyList)))
+print(list(filter(lambda x:True if isinstance(x, bool) else False, MyList)))
