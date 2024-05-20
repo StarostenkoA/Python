a932f23b321d67250705a7ff153a3d3a1997035b
StarostenkoA <167009338+StarostenkoA@users.noreply.github.com>
2024-05-20 17:20:18 +0300

    Update lesson10_6.py

--- lesson10/lesson10_6.py
+++ lesson10/lesson10_6.py
@@ -1,1 +1,21 @@
+def counter(n):
+    i = n
+    
+    def func():
+        nonlocal i
+        i += 1
+        return i
+    
+    return func
 
+
+c1 = counter(1)
+print(c1())  # 1
+print(c1())  # 2
+print(c1())  # 3
+
+
+c2 = counter(10)
+print(c2())  # 1
+print(c2())  # 2
+print(c2())  # 3
