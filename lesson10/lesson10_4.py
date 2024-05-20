fee685f4a34489fae8ad3f84dea70bec64d2689e
StarostenkoA <167009338+StarostenkoA@users.noreply.github.com>
2024-05-20 17:19:05 +0300

    Update lesson10_4.py

--- lesson10/lesson10_4.py
+++ lesson10/lesson10_4.py
@@ -1,1 +1,14 @@
+def ex_decorator(func):
+    def wrapper(*args,**kwargs):
+        try:
+            func(*args,**kwargs)
+        except BaseException as e:
+            print(f"in finction {func.__name__} error - {str(e)}")
+        
+    return wrapper
 
+@ex_decorator
+def exept():
+    error=7/0
+    
+exept()
