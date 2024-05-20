c2d768e2535af54d3955c3a0b06c43ad74ea4a20
StarostenkoA <167009338+StarostenkoA@users.noreply.github.com>
2024-05-20 17:18:33 +0300

    Update lesson10_3.py

--- lesson10/lesson10_3.py
+++ lesson10/lesson10_3.py
@@ -1,1 +1,12 @@
+def log_decorator(func):
+    def wrapper(*args,**kwargs):
+        print(f"Executing function {func.__name__} with arguments args-{args} kwargs- {kwargs}")
+        func(*args,**kwargs)
+        print(f"{func.__name__} - completed")
+    return wrapper
 
+@log_decorator
+def hello(name):
+    print(f"Hello, {name}")
+    
+hello("Vasia")
