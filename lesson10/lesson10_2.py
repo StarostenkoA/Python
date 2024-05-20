d162aca54a29fcf07c5c1c32c2dbba3a0aa5e242
StarostenkoA <167009338+StarostenkoA@users.noreply.github.com>
2024-05-20 17:17:57 +0300

    Update lesson10_2.py

--- lesson10/lesson10_2.py
+++ lesson10/lesson10_2.py
@@ -1,1 +1,45 @@
+def bread(func):
+    def wrapper():
+        print("</------------\\>")
+        func()
+        print("<\\____________/>")
+    return wrapper
 
+def tomato(func):
+    def wrapper():
+        print("***  tomato  ****")
+        func()
+    return wrapper
+
+def salad(func):
+    def wrapper():
+        print("~~~~ salad ~~~~~")
+        func()
+    return wrapper
+
+def cheese(func):
+    def wrapper():
+        print("^^^^^cheese^^^^^")
+        func()
+    return wrapper
+
+def onion(func):
+    def wrapper():
+        print("-----onion------")
+        func()
+    return wrapper
+
+@bread
+@onion
+@tomato
+def beef():
+    print("###   beef   ###")
+
+@bread
+@cheese
+@salad    
+def chicken():
+    print("|||| chicken||||")
+   
+
+chicken()
