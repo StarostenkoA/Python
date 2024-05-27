2e0ba054be6f900907fbd9307557ded3e3c34115
StarostenkoA <167009338+StarostenkoA@users.noreply.github.com>
2024-05-27 13:31:17 +0300

    Update lesson11_1.py

--- lesson11/lesson11_1.py
+++ lesson11/lesson11_1.py
@@ -1,1 +1,27 @@
 
+class Phone:
+    brand = ""
+    model = ""
+    issue_year = ''
+
+    def __init__(self, brand, model,issue_year) -> None:
+        self.brand = brand
+        self.model = model
+        self.issue_year = issue_year
+    
+    def receive_call(self, name):
+        print(f"<{self.brand}-{self.model}> - Call {name}")
+    
+    def get_info(self):
+       return (self.brand, self.model,self.issue_year)
+    
+    def __str__(self):    
+        return f" Brand: {self.brand}\n Model: {self.model}\n Issue year: {self.issue_year}\n"
+        
+        
+
+samsung = Phone('samsung', 'A50', '2022')    
+
+samsung.receive_call('Alexandr')
+print(samsung.get_info())
+print(samsung)
